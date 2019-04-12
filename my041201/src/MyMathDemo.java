3ce7d1b013f4865c29352770ff04a449414a4762
ZHY <liufuling14@163.com>
2019-04-12 10:35:04 +0800

    20190412

---
+++ my041201/src/MyMathDemo.java
@@ -0,0 +1,10 @@
+public class MyMathDemo {
+    public static void main(String [] args){
+
+        mymath myma=new mymath();
+        System.out.println("加法 ："+myma.add(23,34));
+        System.out.println("减法 ："+myma.subtract(23,34));
+        System.out.println("乘法 ："+myma.multiply(23,34));
+        System.out.println("除法 ："+myma.divide(10,4));
+    }
+}
